cc30609753d55b04058e24e1dd390952107cf923
Isabela Porto <isamporto@hotmail.com>
2020-06-01 00:07:47 -0300

    Fixed background color and yarn add select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap';
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  initSelect2();
+});
 
 require("@rails/ujs").start()
 require("turbolinks").start()
